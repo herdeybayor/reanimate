1a549b3e6ba2ba0d2f331bd10428ab71ba2d917b
herdeybayor <shereefdeen4real@gmail.com>
2024-11-07 14:11:48 +0100

    Refactor pan gesture handling and add animation to square

--- spatial-tap-gestures/src/index.tsx
+++ spatial-tap-gestures/src/index.tsx
@@ -1,21 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  cancelAnimation,
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+const CIRCLE_RADIUS = 30;
 
 const App = () => {
+  const left = useSharedValue(0);
+  const top = useSharedValue(0);
+  const scale = useSharedValue(0);
+
+  const tapGesture = Gesture.Tap().onEnd(event => {
+    left.value = event.x - CIRCLE_RADIUS;
+    top.value = event.y - CIRCLE_RADIUS;
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      left: left.value,
+      top: top.value,
+      transform: [{ scale: scale.value }],
+    };
+  }, []);
+
+  useAnimatedReaction(
+    () => {
+      return left.value;
+    },
+    (curr, prev) => {
+      if (prev && curr !== prev) {
+        cancelAnimation(scale);
+        scale.value = 0;
+        scale.value = withSpring(1);
+      }
+    },
+    [left],
+  );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureDetector gesture={tapGesture}>
+      <Animated.View style={styles.container}>
+        <StatusBar style="light" />
+        <Animated.View style={[styles.baseCircle, rStyle]} />
+      </Animated.View>
+    </GestureDetector>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  baseCircle: {
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    borderRadius: CIRCLE_RADIUS,
+    backgroundColor: '#2f2f2f',
+    position: 'absolute',
   },
 });
 
