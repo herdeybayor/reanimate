4b4c022447d4f04154c1b01732f7cf9415b02b12
herdeybayor <shereefdeen4real@gmail.com>
2024-11-07 13:49:33 +0100

    Add tsconfig.json, .gitignore, .prettierrc.js, babel.config.js, README.md, App.tsx, .eslintrc.js, src/index.tsx, app.json, and package.json

---
+++ spatial-tap-gestures/App.tsx
@@ -0,0 +1,6 @@
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
+import { App } from './src';
+
+// eslint-disable-next-line import/no-default-export
+export default gestureHandlerRootHOC(App);
