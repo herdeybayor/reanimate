df04838ec13d955a0a41dcf18ca7f9070c092b81
herdeybayor <shereefdeen4real@gmail.com>
2024-11-07 13:08:45 +0100

    Refactor pan gesture handling and add animation to square

--- pan-gesture-handler/src/index.tsx
+++ pan-gesture-handler/src/index.tsx
@@ -3,7 +3,10 @@ import { StyleSheet, View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
+  withSpring,
+  withTiming,
 } from 'react-native-reanimated';
 
 const SQUARE_SIZE = 120;
@@ -13,20 +16,60 @@ const App = () => {
   const translateY = useSharedValue(0);
   const context = useSharedValue({ x: 0, y: 0 });
 
+  const isDragging = useSharedValue(false);
+
   const panGesture = Gesture.Pan()
     .onBegin(() => {
+      isDragging.value = true;
       context.value = { x: translateX.value, y: translateY.value };
     })
     .onUpdate(({ translationX, translationY }) => {
       translateX.value = translationX + context.value.x;
       translateY.value = translationY + context.value.y;
+    })
+    .onFinalize(() => {
+      isDragging.value = false;
     });
+
+  const rotate = useDerivedValue(() => {
+    return withSpring(isDragging.value ? '45deg' : '0deg');
+  }, [isDragging]);
+
+  const scale = useDerivedValue(() => {
+    return withTiming(isDragging.value ? 0.9 : 1);
+  }, [isDragging]);
+
+  const color = useDerivedValue(() => {
+    if (isDragging.value) {
+      return '#0099ff';
+    }
+
+    const isInTheWhiteArea = translateY.value < 0;
+    const isInTheBlackArea = translateY.value > 0;
+
+    if (isInTheWhiteArea) {
+      return '#000';
+    }
+
+    if (isInTheBlackArea) {
+      return '#fff';
+    }
+
+    return '#0099ff';
+  }, [isDragging]);
+
+  const animatedColor = useDerivedValue(() => {
+    return withTiming(color.value);
+  }, [color]);
 
   const rStyle = useAnimatedStyle(() => {
     return {
+      backgroundColor: animatedColor.value,
       transform: [
         { translateX: translateX.value },
         { translateY: translateY.value },
+        { scale: scale.value },
+        { rotate: rotate.value },
       ],
     };
   }, []);
@@ -52,7 +95,6 @@ const styles = StyleSheet.create({
   square: {
     width: SQUARE_SIZE,
     height: SQUARE_SIZE,
-    backgroundColor: '#0099ff',
     borderRadius: 30,
     borderCurve: 'continuous',
   },
